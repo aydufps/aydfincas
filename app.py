4c9c412e739991823baad55e06f01f08847406c0
Jeferson Hernandez <j.hernandez@nelumboconsultores.com>
2022-06-04 18:31:36 -0500

    cambio en la configuracion para hace rdeploy en heroku

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
-from flask_restful import reqparse
+from flask_restful import reqparse, Resource
+
 from src.administrador.domain.services.usuarios import loadusers
 from src.administrador.domain.services.roles import loadroles
 from src.auth.domain.services.login import loadauth
@@ -12,5 +13,16 @@ loadauth()
 loadroles()
 loadusers()
 
+
+class status (Resource):
+    def get(self):
+        try:
+            return {'data': 'Api is Running'}
+        except:
+            return {'data': 'An Error Occurred during fetching Api'}
+
+
+api.add_resource(status, '/')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
