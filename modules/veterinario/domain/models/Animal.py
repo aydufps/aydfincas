14749c6d2f43c53daee42dd8011fb5cb4617b59d
Jeferson Hernandez <j.hernandez@nelumboconsultores.com>
2022-06-12 18:28:18 -0500

    feat: agregados campos a la entidad animal

--- modules/veterinario/domain/models/Animal.py
+++ modules/veterinario/domain/models/Animal.py
@@ -10,6 +10,9 @@ class Animal(db.Model):
     nombre = db.Column(db.String(50), nullable=False)
     detalles = db.Column(db.String(100), nullable=False, default="")
     estado = db.Column(db.Boolean, nullable=False, default=True)
+    padre_id = db.Column(db.Integer, db.ForeignKey("animales.id"))
+    madre_id = db.Column(db.Integer, db.ForeignKey("animales.id"))
+    fecha_nacimiento = db.Column(db.Date, nullable=False)
     vacunas = db.relationship("AnimalVacuna")
     created_at = db.Column(
         db.DateTime, server_default=db.func.current_timestamp())
@@ -20,6 +23,9 @@ class Animal(db.Model):
             "nombre": self.nombre,
             "detalles": self.detalles,
             "estado": self.estado,
+            "padre_id": self.padre_id,
+            "madre_id": self.madre_id,
             "vacunas": [i.asJSON() for i in self.vacunas],
             "create_at": str(self.created_at.strftime('%Y-%m-%d')),
+            "fecha_nacimiento": str(self.fecha_nacimiento.strftime('%Y-%m-%d')),
         }
--- modules/veterinario/domain/services/animales.py
+++ modules/veterinario/domain/services/animales.py
@@ -1,8 +1,18 @@
+import argparse
+from datetime import date, datetime
 from flask_restful import Resource, reqparse
 from index import api, db
 from modules.administrador.domain.models.Rol import Rol
 from modules.shared.infrastructure.repositories.parsemodel import hasRequiredFields, parsemodel
 from modules.veterinario.domain.models.Animal import Animal
+
+
+def valid_date(s):
+    try:
+        return datetime.strptime(s, "%Y-%m-%d")
+    except ValueError:
+        msg = "Not a valid date: '{0}'.".format(s)
+        raise argparse.ArgumentTypeError(msg)
 
 
 class Animales(Resource):
@@ -11,15 +21,21 @@ class Animales(Resource):
 
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('nombre', type=str)
-        parser.add_argument('detalles', type=str)
+        parser.add_argument('nombre', type=str, required=True,
+                            help="El campo nombre es obligatorio")
+        parser.add_argument('padre_id', type=str, required=True,
+                            help="El campo padre_id es obligatorio")
+        parser.add_argument('madre_id', type=str, required=True,
+                            help="El campo madre_id es obligatorio")
+        parser.add_argument('detalles', type=str, required=True,
+                            help="El campo detalles es obligatorio")
+        parser.add_argument('fecha_nacimiento', type=valid_date, required=True,
+                            help="El campo fecha_nacimiento es obligatorio")
         args = parser.parse_args()
-        isValid = hasRequiredFields(args, ["nombre", "detalles"])
-        if not isValid:
-            return None, 400
         nombre = args['nombre']
         detalles = args['detalles']
-        animal = Animal(nombre=nombre, detalles=detalles)
+        animal = Animal(nombre=nombre, detalles=detalles,
+                        padre_id=args["padre_id"], madre_id=args["madre_id"], fecha_nacimiento=args["fecha_nacimiento"])
         try:
             db.session.add(animal)
             db.session.commit()
