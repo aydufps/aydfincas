b0431f49095710344fd9f79751ef10809798b196
Jeferson Hernandez <j.hernandez@nelumboconsultores.com>
2022-06-19 09:47:40 -0500

    cambio de clave

--- modules/shared/domain/services/mail.py
+++ modules/shared/domain/services/mail.py
@@ -3,6 +3,7 @@ from index import api, mail
 from flask_mail import Message
 from flask_restful import Resource, reqparse
 from modules.administrador.domain.models.Usuario import Usuario
+from index import db
 
 from modules.shared.infrastructure.repositories.SendEmail import enviarEmail
 # from modules.shared.infrastructure.repositories.SendEmail import enviarEmail
@@ -27,5 +28,25 @@ class Mail(Resource):
         return "No existe este usuario"
 
 
+class MailRouter(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('clave', type=str, required=True,
+                            help="El campo clave es obligatorio")
+        parser.add_argument('id', type=int, required=True,
+                            help="El campo id es obligatorio")
+        args = parser.parse_args()
+        item = Usuario.query.get_or_404(args['id'])
+        item.clave = args['clave']
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            return None, 400
+        return f'{item.name.capitalize()}, Se ha actualizado tu clave', 200
+
+
 def loadmail():
     api.add_resource(Mail, '/mail')
+    api.add_resource(MailRouter, '/nueva_clave')
