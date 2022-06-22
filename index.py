40e3549d2c7657bd60328b49b5b4dafabf5a728a
Jeferson Hernandez <j.hernandez@nelumboconsultores.com>
2022-06-21 20:51:10 -0500

    updated

--- index.py
+++ index.py
@@ -11,9 +11,9 @@ api = Api(app)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 mail = Mail(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(
-    os.path.join(os.path.dirname(__file__), 'app.db'))
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://tigersjo_develop:Soporte2014@tigersjob.com.co/tigersjo_fincas'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(
+#    os.path.join(os.path.dirname(__file__), 'app.db'))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://tigersjo_develop:Soporte2014@tigersjob.com.co/tigersjo_fincas'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 ''' app.config['SQLALCHEMY_ENGINE_OPTIONS '] = {'mysql_engine': 'InnoDB'} '''
 convention = {
