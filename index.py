c20c62c8470addb5ce78b7c004584bab810d3dec
Jeferson Hernandez <j.hernandez@nelumboconsultores.com>
2022-06-21 21:43:53 -0500

    agregado plugion postgres

--- index.py
+++ index.py
@@ -10,10 +10,13 @@ app.debug = False
 api = Api(app)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 mail = Mail(app)
+app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://rwoknghjanqvdf:9730eeb60bfb038f183fc4583bbef8e63046d70534f058faf6b988bd36428078@ec2-34-200-35-222.compute-1.amazonaws.com:5432/d22op733jj8jo3"
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(
 #    os.path.join(os.path.dirname(__file__), 'app.db'))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://tigersjo_develop:Soporte2014@tigersjob.com.co/tigersjo_fincas'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://tigersjo_develop:Soporte2014@tigersjob.com.co/tigersjo_fincas'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 ''' app.config['SQLALCHEMY_ENGINE_OPTIONS '] = {'mysql_engine': 'InnoDB'} '''
 convention = {
